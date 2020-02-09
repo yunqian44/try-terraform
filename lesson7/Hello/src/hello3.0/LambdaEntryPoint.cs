cae72af9612c088811f1a4430d45777ae080086b
yrzhang <yrzhang@thoughtworks.com>
2020-02-09 19:39:25 +0800

    play dotnet core and lambda

---
+++ lesson7/Hello/src/hello3.0/LambdaEntryPoint.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Hosting;
+
+namespace hello3
+{
+    /// <summary>
+    /// This class extends from APIGatewayProxyFunction which contains the method FunctionHandlerAsync which is the 
+    /// actual Lambda function entry point. 
+    /// </summary>
+    public class LambdaEntryPoint :
+        // When using an ELB's Application Load Balancer as the event source change 
+        // the base class to Amazon.Lambda.AspNetCoreServer.ApplicationLoadBalancerFunction
+        Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+    {
+        /// <summary>
+        /// The builder has configuration, logging and Amazon API Gateway already configured. The startup class
+        /// needs to be configured in this method using the UseStartup<>() method.
+        /// </summary>
+        /// <param name="builder"></param>
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder
+                .UseStartup<Startup>();
+        }
+    }
+}
---
+++ lesson7/Hello/test/hello3.0.Tests/WeatherForecastControllerTests.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xunit;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.TestUtilities;
+using Amazon.Lambda.APIGatewayEvents;
+
+using Newtonsoft.Json;
+
+using hello3._0;
+
+
+namespace hello3._0.Tests
+{
+    public class WeatherForecastControllerTests
+    {
+
+
+        [Fact]
+        public async Task TestGet()
+        {
+            var lambdaFunction = new LambdaEntryPoint();
+
+            var requestStr = File.ReadAllText("./SampleRequests/WeatherForecastController-Get.json");
+            var request = JsonConvert.DeserializeObject<APIGatewayProxyRequest>(requestStr);
+            var context = new TestLambdaContext();
+            var response = await lambdaFunction.FunctionHandlerAsync(request, context);
+
+            Assert.Equal(200, response.StatusCode);
+            Assert.Contains("temperaturec", response.Body.ToLower());
+            Assert.True(response.MultiValueHeaders.ContainsKey("Content-Type"));
+            Assert.Equal("application/json; charset=utf-8", response.MultiValueHeaders["Content-Type"][0]);
+        }
+
+
+    }
+}
