7f7e060a67324803b126713b779a45f092a425ee
yrzhang <yrzhang@thoughtworks.com>
2020-03-01 00:17:24 +0800

    simple vpc

---
+++ vpc/simple-vpc/main.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region = "ap-east-1"
+}
+
+data "aws_security_group" "default" {
+  name   = "default"
+  vpc_id = module.vpc.vpc_id
+}
+
+module "vpc" {
+  source = "../modules"
+
+  name = "simple-example"
+
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-east-1a", "ap-east-1b", "ap-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_ipv6 = true
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  public_subnet_tags = {
+    Name = "overridden-name-public"
+  }
+
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
+
+  vpc_tags = {
+    Name = "vpc-name"
+  }
+}
