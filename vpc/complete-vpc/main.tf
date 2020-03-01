57135b4c51a4e35af44a812206fb42e6d36f20c7
yrzhang <yrzhang@thoughtworks.com>
2020-03-01 17:13:32 +0800

    play acls

--- vpc/complete-vpc/main.tf
+++ vpc/complete-vpc/main.tf
@@ -17,18 +17,11 @@ module "vpc" {
   azs                 = ["ap-east-1a", "ap-east-1b", "ap-east-1c"]
   private_subnets     = ["20.10.1.0/24", "20.10.2.0/24", "20.10.3.0/24"]
   public_subnets      = ["20.10.11.0/24", "20.10.12.0/24", "20.10.13.0/24"]
-  database_subnets    = ["20.10.21.0/24", "20.10.22.0/24", "20.10.23.0/24"]
-  elasticache_subnets = ["20.10.31.0/24", "20.10.32.0/24", "20.10.33.0/24"]
-  redshift_subnets    = ["20.10.41.0/24", "20.10.42.0/24", "20.10.43.0/24"]
-  intra_subnets       = ["20.10.51.0/24", "20.10.52.0/24", "20.10.53.0/24"]
 
   create_database_subnet_group = false
 
   enable_dns_hostnames = true
   enable_dns_support   = true
-
-  enable_classiclink             = true
-  enable_classiclink_dns_support = true
 
   enable_nat_gateway = true
   single_nat_gateway = true
